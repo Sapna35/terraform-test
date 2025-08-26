c70a04302ab26c87e01636994033dca9fa4b3e41
Sapna Rani <105926720+Sapna35@users.noreply.github.com>
2025-08-26 18:32:15 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,92 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+# Fetch default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Security Group for Jenkins Master
+resource "aws_security_group" "jenkins_sg" {
+  name        = "jenkins-sg"
+  description = "Allow SSH and Jenkins"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Jenkins"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = { Name = "jenkins-sg" }
+}
+
+# Security Group for App Node
+resource "aws_security_group" "app_sg" {
+  name        = "app-sg"
+  description = "Allow SSH and Tomcat"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Tomcat"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = { Name = "app-sg" }
+}
+
+# Jenkins Master EC2
+resource "aws_instance" "jenkins_master" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
+
+  tags = { Name = "Jenkins-Master" }
+}
+
+# Application Node EC2
+resource "aws_instance" "app_node" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  vpc_security_group_ids = [aws_security_group.app_sg.id]
+
+  tags = { Name = "Application-Node" }
+}
